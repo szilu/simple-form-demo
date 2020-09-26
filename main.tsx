c915951624746db681f950eca8b58155f810c462
Szilard Hajba <szilu@symbion.hu>
2020-09-26 19:41:27 +0200

    Upgrade simple-form library to v0.3.0

--- main.tsx
+++ main.tsx
@@ -1,28 +1,16 @@
 import * as React from 'react'
 import { render } from 'react-dom'
 
-import { useForm, withForm } from '@symbion/simple-form'
-import * as t from '@symbion/simple-form/lib/types'
-import * as F from '@symbion/simple-form/lib/components-withform'
+import { useForm, withForm, t } from '@symbion/simple-form'
+import typedInputs from '@symbion/simple-form/lib/components-withform'
+import { V } from '@symbion/simple-form/lib/validator'
 
-// Some branding types for validation
-/////////////////////////////////////
-interface Name { readonly Name: unique symbol }
-const Name = t.brand(t.string, (s): s is t.Branded<string, Name> => s.length >= 4, 'Name')
+// Validator functions
+//////////////////////
+const isFutureDate: t.Validator<string> = async (value: string) => new Promise(resolve => setTimeout(() => resolve(typeof value == 'string' && Date.parse(value) > Date.now()), 1000))
 
-interface Email { readonly Email: unique symbol }
-const Email = t.brand(t.string, (s): s is t.Branded<string, Email> =>
-	/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(s),
-	'Email')
-
-interface PosInt { readonly PosInt: unique symbol }
-const PosInt = t.brand(t.number, (n): n is t.Branded<number, PosInt> => Number.isInteger(n) && n >= 0, 'PosInt')
-
-interface DateT { readonly Date: unique symbol }
-const DateT = t.brand(t.string, (s): s is t.Branded<string, DateT> => !Number.isNaN(Date.parse(s)), 'Date')
-
-interface DateFuture { readonly DateFuture: unique symbol }
-const DateFuture = t.brand(t.string, (s): s is t.Branded<string, DateFuture> => Date.parse(s) > Date.now(), 'DateFuture')
+// Validator class
+//////////////////
 
 // Form data model
 //////////////////
@@ -35,24 +23,24 @@ const DateFuture = t.brand(t.string, (s): s is t.Branded<string, DateFuture> => 
  * In the example we use basic types for BASE and branding types for STRICT.
  * STRICT can be omitted.
  */
-export const TProfile = t.formModel({
-	name: { base: t.string, strict: Name },
-	email: { base: t.string, strict: Email },
-	password: { base: t.string, strict: Name },
-	age: { base: t.number, strict: t.optional(PosInt) },
-	range: { base: t.number, strict: t.optional(PosInt) },
-	date: { base: DateT, strict: DateFuture },
-	agree: { base: t.boolean, strict: t.trueType },
-	vote: { base: t.string, strict: t.literals('1', '2', '3') },
-	vote2: { base: t.string, strict: t.literals('A', 'B', 'C') },
-	vote3: { base: t.number, strict: t.literals('1', '2', '3') },
-	color: { base: t.string }
+export const profileModel = t.formModel({
+	name: { type: t.string, valid: V.string().minLength(4) },
+	email: { type: t.string, valid: V.string().email() },
+	password: { type: t.string, valid: V.string().minLength(8) },
+	age: { type: t.optional(t.number), valid: V.number() },
+	range: { type: t.number, valid: V.number().positive().integer() },
+	date: { type: t.optional(t.string), valid: isFutureDate },
+	agree: { type: t.boolean, valid: V.boolean().true() },
+	vote: { type: t.string, valid: V.string().in(['1', '2', '3']) },
+	vote2: { type: t.string, valid: V.string().in(['A']) },
+	vote3: { type: t.number, valid: V.number().in([2]) },
+	color: { type: t.string }
 })
 
 /* The formModel helper provides two io-ts types for the model
  */
-type TProfileBase = t.TypeOf<typeof TProfile.base>
-type TProfileStrict = t.TypeOf<typeof TProfile.strict>
+type TProfilePartial = t.TypeOf<typeof profileModel.base>
+type TProfile = t.TypeOf<typeof profileModel.strict>
 
 /* These are just some values for demonstration.
  */
@@ -63,19 +51,8 @@ const initialFormValues = {
 
 /* For strict type checking we have to create specific components that know
  * about our model type.
- *
- * If you are lazy, you can use generic components also exported from the
- * library.
  */
-const TextInput = F.createTextInput<TProfileStrict>()
-const NumberInput = F.createNumberInput<TProfileStrict>()
-const DateInput = F.createDateInput<TProfileStrict>()
-const CheckBox = F.createCheckBox<TProfileStrict>()
-const Radio = F.createRadio<TProfileStrict>()
-const NumberRadio = F.createNumberRadio<TProfileStrict>()
-const Select = F.createSelect<TProfileStrict>()
-const NumberSelect = F.createNumberSelect<TProfileStrict>()
-const ColorInput = F.createColorInput<TProfileStrict>()
+const F = typedInputs<TProfile>()
 
 // Form component
 /////////////////
@@ -85,22 +62,22 @@ function ProfileForm() {
 	 *
 	 * syntax useForm< FormModelType > (FormModelRuntimeType, options)
 	 */
-	const form = useForm<TProfileStrict>(TProfile.strict, { formID: 'profile' })
+	const form = useForm<TProfile>(profileModel, { formID: 'profile' })
 
 	/* We could initialize the form immediately, but it's more interesting to emulate an async form loading with an Effect.
 	 */
 	React.useEffect(() => {
 		setTimeout(() => {
-			const res = TProfile.base.decode(initialFormValues)
-			if (t.isRight(res)) form.set(res.right)
+			form.setStrict(initialFormValues)
 		}, 2000)
 	}, [])
 
 	/* This is our onSubmit handler, it's pretty empty.
 	 */
 	async function onSubmit() {
-		console.log('SUBMIT', form.valid() ? form.get() : 'invalid')
-		if (form.valid()) {
+		const valid = await form.valid()
+		console.log('SUBMIT', valid ? form.get() : 'invalid')
+		if (valid) {
 			alert('Form submit\n---------------------\n\n'
 				+ Object.entries(form.get()).map(entry => entry.join(': ')).join('\n'))
 		}
@@ -118,36 +95,36 @@ function ProfileForm() {
 		<h1>Profile</h1>
 		<F.Form onSubmit={onSubmit} onReset={form.reset} form={form}>
 			<F.FieldSet disabled={!form.state} legend='Text inputs'>
-				<TextInput name='name' label='Name' error='Please provide minimum 4 characters' form={form}/>
-				<TextInput name='email' type='email' label='Email' error='Please provide a valid email address' form={form}/>
-				<TextInput name='password' type='password' label='Password' error='Please provide minimum 4 characters' form={form}/>
+				<F.TextInput name='name' label='Name' error='Please provide minimum 4 characters' form={form}/>
+				<F.TextInput name='email' type='email' label='Email' error='Please provide a valid email address' form={form}/>
+				<F.TextInput name='password' type='password' label='Password' error='Please provide minimum 8 characters' form={form}/>
 			</F.FieldSet>
 			<F.FieldSet disabled={!form.state} legend='Numeric inputs'>
-				<NumberInput name='age' label='Age' error='Please provide a positive integer' form={form}/>
-				<NumberInput name='range' type='range' label='Range' error='X' min={0} max={100} step={1} form={form}/>
+				<F.NumberInput name='age' label='Age' error='Please provide a positive integer' form={form}/>
+				<F.NumberInput name='range' type='range' label='Range' error='X' min={0} max={100} step={1} form={form}/>
 			</F.FieldSet>
 			<F.FieldSet disabled={!form.state} legend='Date inputs'>
-				<DateInput name='date' label='Date' error='Please provide a future date' form={form} min={new Date().toISOString().substr(0, 10)}/>
+				<F.DateInput name='date' label='Date' error='Please provide a future date' form={form} min={new Date().toISOString().substr(0, 10)}/>
 			</F.FieldSet>
 			<F.FieldSet disabled={!form.state} legend='Radio buttons'>
-				<Radio name='vote' radioValue='1' label='Option 1' form={form}/>
-				<Radio name='vote' radioValue='2' label='Option 2' form={form}/>
-				<Radio name='vote' radioValue='3' label='Option 3' form={form} error='Please choose'/>
+				<F.Radio name='vote' radioValue='1' label='Option 1' form={form}/>
+				<F.Radio name='vote' radioValue='2' label='Option 2' form={form}/>
+				<F.Radio name='vote' radioValue='3' label='Option 3' form={form} error='Please choose'/>
 			</F.FieldSet>
 			<F.FieldSet disabled={!form.state} legend='Other inputs'>
-				<Select name='vote2' label='Vote'
+				<F.Select name='vote2' label='Vote'
 					options={[[undefined, 'Please select'], ['A', 'Option A'], ['B', 'Option B'], ['C', 'Option C']]}
-					error='Please select one option'
+					error='Please select option A'
 					form={form}
 				/>
-				<NumberSelect name='vote3' label='Vote'
+				<F.NumberSelect name='vote3' label='Vote'
 					options={[[undefined, 'Please select'], [1, 'Option 1'], [2, 'Option 2'], [3, 'Option 3']]}
-					error='Please select one option'
+					error='Please select option 2'
 					form={form}
 				/>
-				<ColorInput name='color' label='Color' form={form}/>
+				<F.ColorInput name='color' label='Color' form={form}/>
 			</F.FieldSet>
-			<CheckBox name='agree' label='I agree to any conditions :)' error='You must agree to our conditions' form={form}/>
+			<F.CheckBox name='agree' label='I agree to any conditions :)' error='You must agree to our conditions' form={form}/>
 			<F.Button type='submit' className='btn-primary'>Submit</F.Button>
 			<F.Button type='reset' className='btn-secondary'>Reset</F.Button>
 		</F.Form>
